d7a85eb3bcd6a7c11d7a4ba300ecb279d7e7a4fc
Jamie Teunissen <jamie.teunissen@me.com>
2020-11-11 10:52:54 +0100

    Week 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        List<Integer> list = makeRndList(100);
+        int maxDif = determineMaxDifference(list);
+        System.out.println(maxDif);
+
+    }
+
+    private static List<Integer> makeRndList(int numberOfItems){
+        Random rd = new Random(); // creating Random object
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < numberOfItems; i++){
+            resultList.add(rd.nextInt(100));
+        }
+
+        return resultList;
+    }
+
+    private static Integer determineMaxDifference(List<Integer> inputList){
+        int upperBound = 0;
+        int lowerBound = 100;
+        for (int i = 0; i < inputList.size(); i++){
+            int check = inputList.get(i);
+            if (check > upperBound){
+                upperBound = check;
+            }
+            if (check < lowerBound){
+                lowerBound = check;
+            }
+        }
+
+        return upperBound - lowerBound;
     }
 }
