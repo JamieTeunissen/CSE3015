2d5eee757290059bbce06631851d146fc64996a7
Jamie Teunissen <jamie.teunissen@me.com>
2020-11-16 11:50:39 +0100

    videoExcersices Done with Recursive

--- src/com/company/videoExcersices.java
+++ src/com/company/videoExcersices.java
@@ -7,31 +7,31 @@ import java.util.Random;
 
 public class videoExcersices {
     public static void main(String args[]){
-        List<Integer> SRList = makeList(100);
-        List<List<Integer>> answer = splitList(SRList);
-        System.out.println(answer);
+        List<Integer> SRList = makeRndOrderList(100);
+        System.out.println(SRList);
+        int splitPos = splitListPos(50, SRList.size()/2, SRList);
+        System.out.println(splitPos);
+
+        System.out.println("Lower List: " + SRList.subList(0, splitPos));
+        System.out.println("Upper List: " + SRList.subList(splitPos + 1, SRList.size()));
     }
 
-    private static List<Integer> makeList(int numberOfItems){
+    private static List<Integer> makeRndOrderList(int numberOfItems){
         List<Integer> resultList = new ArrayList<>();
+        Random rd = new Random();
         for (int i = 0; i < numberOfItems; i++){
-            resultList.add(i);
+            if (rd.nextInt(10) == 1){
+                resultList.add(i);
+            }
         }
         return resultList;
     }
 
-    private static List<List<Integer>> splitList(List<Integer> sList){
-        int listSize = sList.size();
-        int splitValue = listSize / 2;
-
-        List<Integer> list1 = sList.subList(0, splitValue);
-        List<Integer> list2 = sList.subList(splitValue + 1, listSize);
-
-        List<List<Integer>> resultList = new ArrayList<>(2);
-        resultList.add(list1);
-        resultList.add(list2);
-
-
-        return resultList;
+    private static int splitListPos(int splitValue, int splitPosition ,  List<Integer> sList){
+        if (sList.get(splitPosition) > splitValue){
+            return splitListPos(splitValue, splitPosition-1, sList);
+        }else{
+            return splitPosition + 1;
+        }
     }
 }
