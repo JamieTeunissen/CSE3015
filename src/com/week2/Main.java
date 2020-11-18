50755eb3295bfc8e3ae211fcdca7a7b452247939
Jamie Teunissen <jamie.teunissen@me.com>
2020-11-18 20:09:33 +0100

    Weblab 3

--- src/com/week2/Main.java
+++ src/com/week2/Main.java
@@ -1,53 +1,58 @@
 package com.week2;
+import java.util.LinkedList;
+import java.util.List;
 
-import java.util.ArrayList;
+class Queue<T> {
+    private List<T> list = new LinkedList<T>();
 
-import static java.util.Arrays.*;
+    public Queue() {
+
+    }
+
+    /**
+     * @return true iff the queue contains no elements.
+     */
+    public boolean isEmpty() {
+        if(list.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Adds an element to the front of the queue.
+     *
+     * @param element
+     *     to add.
+     */
+    public void enqueue(T element) {
+        list.add(element);
+    }
+
+    /**
+     * Removes and returns the element at the front of the queue.
+     *
+     * @return the element at the front of the dequeue
+     * @throws EmptyQueueException
+     *     iff the queue is empty
+     */
+    public T dequeue() {
+        if (isEmpty()){
+            return null;
+        }else{
+            T element = list.get(0);
+            list.remove(0);
+            return element;
+        }
+    }
+}
 
 public class Main {
-
     public static void main(String[] args) {
-        ArrayList<Integer> input = new ArrayList<>(asList(1, 1, 3, 5, 7, 1, 5, 9, 1));
-        removeAll(input);
-        System.out.println("Fill");
+        Queue<Integer> queue = new Queue<>();
+        queue.isEmpty();
+        queue.enqueue(5);
+        queue.isEmpty();
+        System.out.println(queue.dequeue());
     }
-
-    /**
-     * Removes all elements from the ArrayList, using the removeLastOccurrence method.
-     *
-     * @param list
-     *     to remove the elements from.
-     */
-    public static void removeAll(ArrayList<Integer> list) {
-        int cycleAmount = list.size();
-        for (int i = 0; i < cycleAmount; i++){
-            removeLastOccurrence(list.get(0), list);
-        }
-    }
-
-    /**
-     * Takes an ArrayList and removes last occurrence of x,
-     * shifting the rest of the elements left.
-     * I.e. [5, 1, 5, 9, 8], with x = 5
-     * would result in: [5, 1, 9, 8].
-     * Note that this method does not return a new list.
-     * Instead, the list that is passed as a parameter is changed.
-     *
-     * @param list
-     *     to remove an element from.
-     * @param x
-     *     element value to look for
-     */
-    public static void removeLastOccurrence(int x, ArrayList<Integer> list) {
-        int positionLastOccurrence = -1;
-        for (int i = 0; i < list.size(); i++){
-            if (list.get(i) == x){
-                positionLastOccurrence = i;
-            }
-        }
-        if(positionLastOccurrence >= 0){
-            list.remove(positionLastOccurrence);
-        }
-    }
-
 }
