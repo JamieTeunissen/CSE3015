0fa878eaaf0d369e707bb1849703fde646f97cd7
Jamie Teunissen <jamie.teunissen@me.com>
2020-11-18 20:45:21 +0100

    Weblab 4

--- src/com/week2/Main.java
+++ src/com/week2/Main.java
@@ -1,58 +1,24 @@
 package com.week2;
-import java.util.LinkedList;
-import java.util.List;
-
-class Queue<T> {
-    private List<T> list = new LinkedList<T>();
-
-    public Queue() {
-
-    }
-
-    /**
-     * @return true iff the queue contains no elements.
-     */
-    public boolean isEmpty() {
-        if(list.size() == 0){
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Adds an element to the front of the queue.
-     *
-     * @param element
-     *     to add.
-     */
-    public void enqueue(T element) {
-        list.add(element);
-    }
-
-    /**
-     * Removes and returns the element at the front of the queue.
-     *
-     * @return the element at the front of the dequeue
-     * @throws EmptyQueueException
-     *     iff the queue is empty
-     */
-    public T dequeue() {
-        if (isEmpty()){
-            return null;
-        }else{
-            T element = list.get(0);
-            list.remove(0);
-            return element;
-        }
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new Queue<>();
-        queue.isEmpty();
-        queue.enqueue(5);
-        queue.isEmpty();
-        System.out.println(queue.dequeue());
+        double[][] array = new double[0][0];
+        double[][] clone = clone(array);
+    }
+
+    static double[][] clone(double[][] a) {
+        if(a.length == 0){
+            return a;
+        }
+        double[][] b = new double[a.length][a[0].length];
+        int iterator = 0;
+        for (double[] row : a){
+            for (int i = 0; i < row.length; i++){
+                b[iterator][i] = row[i];
+            }
+            iterator++;
+        }
+
+        return b;
     }
 }
