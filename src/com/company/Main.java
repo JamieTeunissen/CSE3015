bddf5c1674e4d1f21538b58ba2baae9ac6709138
Jamie Teunissen <jamie.teunissen@me.com>
2020-12-02 20:20:25 +0100

    Weblab 4.1 2 pre req

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,59 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         int[] data = {11, 90, 33, 71, 24, 50, 35, 30, 15, 21};
-        selectionSort(data);
-        System.out.println(data);
-
+        int[] result = mergeSort(data);
+        System.out.println(result);
     }
 
-    public static void selectionSort(int[] elements) {
-        for (int i = 0; i < elements.length; i++){
-            int lowest = elements[i];
-            int position = i;
-            for (int j = i; j < elements.length; j++){
-                if (elements[j] < lowest){
-                    lowest = elements[j];
-                    position = j;
-                }
-            }
-            int buffer = elements[i];
-            elements[i] = lowest;
-            elements[position] = buffer;
+    public static int[] mergeSort(int[] elements) {
+        //base case
+        if(elements.length < 2){
+            return elements;
         }
+
+        //split into two lists
+        int middle = elements.length/2;
+        int[] left = Arrays.copyOfRange(elements, 0, middle);
+        int[] right = Arrays.copyOfRange(elements, middle, elements.length);
+
+        //recursion
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        //merge
+        return merger(left, right);
+    }
+
+    private static int[] merger(int[] left, int[] right){
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        int[] res = new int [left.length + right.length];
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                res[x] = left[i];
+                i++;
+            }else{
+                res[x] = right[j];
+                j++;
+            }
+            x++;
+        }
+        while(i < left.length){
+            res[x] = left[i];
+            i++;
+            x++;
+        }
+        while(j < right.length){
+            res[x] = right[j];
+            j++;
+            x++;
+        }
+        return res;
     }
 }
