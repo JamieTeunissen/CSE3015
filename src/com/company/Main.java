3d87c1e0f6eb4853a97f88095f502a49cd1de80d
Jamie Teunissen <jamie.teunissen@me.com>
2020-12-02 21:49:32 +0100

    Weblab 4.1 4 pre req

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,55 +5,84 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] data = {11, 90, 33, 71, 24, 50, 35, 30, 15, 21};
-        int[] result = mergeSort(data);
-        System.out.println(result);
+        MyQueue<Integer> queue1 = new MyQueue<>();
+        queue1.enqueue(4);
+        queue1.enqueue(2);
+        queue1.enqueue(1);
+
+        MyQueue<Integer> queue2 = new MyQueue<>();
+        queue2.enqueue(6);
+        queue2.enqueue(5);
+        queue2.enqueue(3);
+        queue2.enqueue(2);
+
+        MyQueue<Integer> solution = merge(queue1, queue2);
     }
 
-    public static int[] mergeSort(int[] elements) {
-        //base case
-        if(elements.length < 2){
-            return elements;
-        }
+    public static MyQueue<Integer> merge(MyQueue<Integer> queue1, MyQueue<Integer> queue2) {
+        MyQueue<Integer> result = new MyQueue<Integer>();
+        MyQueue<Integer> q1 = queueCopy(queue1);
+        MyQueue<Integer> q2 = queueCopy(queue2);
+        int size = q1.size() + q2.size();
 
-        //split into two lists
-        int middle = elements.length/2;
-        int[] left = Arrays.copyOfRange(elements, 0, middle);
-        int[] right = Arrays.copyOfRange(elements, middle, elements.length);
-
-        //recursion
-        left = mergeSort(left);
-        right = mergeSort(right);
-
-        //merge
-        return merger(left, right);
-    }
-
-    private static int[] merger(int[] left, int[] right){
-        int i = 0;
-        int j = 0;
-        int x = 0;
-        int[] res = new int [left.length + right.length];
-        while(i < left.length && j < right.length){
-            if(left[i] < right[j]){
-                res[x] = left[i];
-                i++;
+        for(int i = 0; i < size; i++){
+            if(q1.front() != null && q2.front() != null){
+                if(q1.front() > q2.front()){
+                    result.enqueue(q1.dequeue());
+                }else{
+                    result.enqueue(q2.dequeue());
+                }
             }else{
-                res[x] = right[j];
-                j++;
+                break;
             }
-            x++;
         }
-        while(i < left.length){
-            res[x] = left[i];
-            i++;
-            x++;
+
+        while(q1.front() != null){
+            result.enqueue(q1.dequeue());
         }
-        while(j < right.length){
-            res[x] = right[j];
-            j++;
-            x++;
+
+        while(q2.front() != null){
+            result.enqueue(q2.dequeue());
         }
-        return res;
+        return result;
+    }
+
+    private static MyQueue<Integer> queueCopy(MyQueue<Integer> queue){
+        MyQueue<Integer> result = new MyQueue<Integer>();
+        while (result.size() != queue.size()){
+            int buffer = queue.dequeue();
+            result.enqueue(buffer);
+            queue.enqueue(buffer);
+        }
+        return result;
+    }
+
+    static class MyQueue<T> {
+
+        private LinkedList<T> q;
+
+        public MyQueue() {
+            this.q = new LinkedList<>();
+        }
+
+        public void enqueue(T e) {
+            q.add(e);
+        }
+
+        public T dequeue() {
+            return q.poll();
+        }
+
+        public int size() {
+            return q.size();
+        }
+
+        public boolean isEmpty() {
+            return q.isEmpty();
+        }
+
+        public T front() {
+            return q.peek();
+        }
     }
 }
