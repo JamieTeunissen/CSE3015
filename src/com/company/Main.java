951cbb09eaf98924a4d8b85a73abf2d9f76237f9
Jamie Teunissen <jamie.teunissen@me.com>
2020-12-02 19:57:41 +0100

    Weblab 4.1 1 pre req

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        int[] data = {11, 90, 33, 71, 24, 50, 35, 30, 15, 21};
+        selectionSort(data);
+        System.out.println(data);
+
+    }
+
+    public static void selectionSort(int[] elements) {
+        for (int i = 0; i < elements.length; i++){
+            int lowest = elements[i];
+            int position = i;
+            for (int j = i; j < elements.length; j++){
+                if (elements[j] < lowest){
+                    lowest = elements[j];
+                    position = j;
+                }
+            }
+            int buffer = elements[i];
+            elements[i] = lowest;
+            elements[position] = buffer;
+        }
     }
 }
