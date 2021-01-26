a459a6466c718c1c020b23c5fd9aaf74ff94833a
Jamie Teunissen <jamie.teunissen@me.com>
2021-01-26 16:50:32 +0100

    Count verices

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,78 +4,123 @@ import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class Main {
+class Graph {
+    static class Vertex {
+        private int id;
+        private Set<Vertex> neighbours;
 
-
-    class LibraryTree {
-        private int key;
-        private List<LibraryTree> children;
-
-        public LibraryTree(int key) {
-            this.key = key;
-            children = new ArrayList<>();
+        public Vertex(int id) {
+            this.id = id;
+            neighbours = new HashSet<>();
         }
 
-        public LibraryTree(int key, List<LibraryTree> children) {
-            this.key = key;
-            this.children = children;
+        public void addNeighbour(Vertex v) {
+            neighbours.add(v);
         }
 
-        public int getKey() {
-            return this.key;
+        private Collection<Vertex> getNeighbours() {
+            return new ArrayList<>(this.neighbours);
         }
 
-        public List<LibraryTree> getChildren() {
-            return this.children;
+        public int getId() {
+            return this.id;
         }
     }
+
+    Map<Integer, Vertex> vertices;
+
+    public Graph() {
+        this.vertices = new HashMap<>();
+    }
+
+    public void addVertex(Vertex v) {
+        this.vertices.put(v.getId(), v);
+    }
+
+    public Collection<Vertex> getAllVertices() {
+        return new ArrayList<>(this.vertices.values());
+    }
+
+    public List<Vertex> getNeighbours(Vertex v) {
+        List<Vertex> neigh = new ArrayList<>(((Vertex) v).getNeighbours());
+        return neigh;
+    }
+
+    public void addEdge(Vertex v, Vertex w) {
+        v.addNeighbour(w);
+        w.addNeighbour(v);
+    }
+}
+
+public class Main {
 
     @Test
     public void testNull() {
-        LibraryTree tree = null;
-        assertEquals(0, countNodesEvenChildren(tree));
+        Graph g = new Graph();
+        Graph.Vertex v = null;
+        assertEquals(0, countVertices(g, v));
+        g = null;
+        v = new Graph.Vertex(1);
+        assertEquals(0, countVertices(g, v));
     }
 
     @Test
-    public void testRoot() {
-        LibraryTree tree = new LibraryTree(0);
-        assertEquals(1, countNodesEvenChildren(tree));
-    }
-
-    @Test
-    public void testSmall() {
-        LibraryTree tree = new LibraryTree(0, Arrays.asList(new LibraryTree(1), new LibraryTree(2)));
-        assertEquals(3, countNodesEvenChildren(tree));
-    }
-
-    public static void main(String[] args) {
-
+    public void testExample() {
+        Graph g = new Graph();
+        Graph.Vertex v = new Graph.Vertex(1);
+        Graph.Vertex w = new Graph.Vertex(2);
+        Graph.Vertex x = new Graph.Vertex(3);
+        Graph.Vertex y = new Graph.Vertex(4);
+        g.addVertex(v);
+        g.addVertex(w);
+        g.addVertex(x);
+        g.addVertex(y);
+        g.addEdge(v, w);
+        g.addEdge(w, y);
+        assertEquals(3, countVertices(g, v));
+        assertEquals(3, countVertices(g, w));
+        assertEquals(3, countVertices(g, y));
+        assertEquals(1, countVertices(g, x));
     }
 
     /**
-     * Counts the number of nodes with an event number of children.
+     * Counts the number of vertices in the same connected component as v in graph g.
+     * This is done using breadth first search.
      *
-     * @param tree
-     *     The tree to count nodes with an even number of children in.
-     * @return the number of nodes with an even number of children, or 0 if tree is null.
+     * Returns 0 if the graph or vertex is null
+     *
+     * @param g
+     *     The graph to count vertices in.
+     * @param v
+     *     The vertex to start counting at.
+     * @return the number of vertices in the same connected component.
      */
-    public static int countNodesEvenChildren(LibraryTree tree) {
-        if(tree == null){
+    public static int countVertices(Graph g, Graph.Vertex v) {
+        if (g == null || v == null){
             return 0;
         }
 
-        int count = 0;
-        Iterator<LibraryTree> childrenIter = tree.getChildren().iterator();
+        Queue<Graph.Vertex> queue = new LinkedList<>();
+        List<Graph.Vertex> knownVertices = new LinkedList<>();
+        int count = 1;
+        knownVertices.add(v);
+        queue.add(v);
 
-        while(childrenIter.hasNext()){
-            count += countNodesEvenChildren(childrenIter.next());
-        }
-
-        if((tree.getChildren().size() % 2) == 0){
-            return 1 + count;
+        while (!queue.isEmpty()){
+            Graph.Vertex select = queue.remove();
+            Iterator<Graph.Vertex> graphIter = g.getNeighbours(select).iterator();
+            while (graphIter.hasNext()){
+                Graph.Vertex check = graphIter.next();
+                if (!knownVertices.contains(check)){
+                    count++;
+                    knownVertices.add(check);
+                    queue.add(check);
+                }
+            }
         }
 
         return count;
+
     }
 
 
