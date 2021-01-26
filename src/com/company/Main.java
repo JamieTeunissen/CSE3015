980406d3c8b75ab59196f1f04f6a433212035357
Jamie Teunissen <jamie.teunissen@me.com>
2021-01-26 13:14:21 +0100

    final 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,50 +1,49 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] input = {4, 2, 5, 1, 3};
-        mergeSort(input);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, input);
+        String[] arr = {"f","g","h","a","b","a"};
+        int[] res = indexSort(arr);
+//        int[] expected = { 0, 2, 3, 1 };
+//        assertArrayEquals(expected, res);
     }
 
-    public static void mergeSort(int[] arr) {
-        if(arr == null || arr.length < 2){
-            return;
+    /**
+     * Sorts the indices of the array based on the corresponding value in alphabetical order.
+     * Returns null if the input array is null.
+     *
+     * Example: The array ["c","a","b"] will result in [1, 2, 0].
+     *
+     * @param arr array of Strings that stored the values
+     * @return the indices in sorted order
+     */
+    public static int[] indexSort(String[] arr) {
+        if (arr == null){
+            return null;
         }
 
-        int split = arr.length/2;
-        int[] s1 = Arrays.copyOfRange(arr, 0, split);
-        int[] s2 = Arrays.copyOfRange(arr, split, arr.length);
+        int[] result = new int[arr.length];
 
-        mergeSort(s1);
-        mergeSort(s2);
-
-        merge(s1, s2, arr);
-
-    }
-
-    public static void merge(int[] s1, int[] s2, int[] arr){
-        int i = 0;
-        int j = 0;
-
-        while (i+j< arr.length){
-            if (j == s2.length || (i < s1.length && s1[i] < s2[j])){
-                arr[i+j] = s1[i];
-                i++;
-            }else{
-                arr[i+j] = s2[j];
-                j++;
+        int j, k;
+        for (j = 1; j < arr.length; j++){
+            String temp = arr[j];
+            int r = j;
+            for(k = j - 1; k >= 0 && arr[k].compareTo(temp) > 0; k--){
+                arr[k+1] = arr[k];
+                result[k+1] = result[k];
             }
+
+            arr[k+1] = temp;
+            result[k+1] = j;
         }
+
+        return result;
+
     }
-
-
 
 }
